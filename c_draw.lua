2a70ec0dfb11e2bfcc20532e3d1852f585165f6f
GlitchyBoi <35081165+GlitchyBoi@users.noreply.github.com>
2019-05-20 11:47:43 +0100

    Create c_draw.lua

---
+++ c_draw.lua
@@ -0,0 +1,9 @@
+--[[
+---------------------------------------------------
+CLIENT DRAWS (C_DRAW.LUA) by GlitchyBoi
+Made for Justice Community Roleplay
+---------------------------------------------------
+HTTPS://GITHUB.COM/GLITCHYBOI/JCRP-REWRITE
+---------------------------------------------------
+--]]
+
